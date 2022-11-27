38b79e3f5f60b463fff42dfcd1586a91624493a0
gyejin <jkj94627@gmail.com>
2022-11-27 17:45:55 +0900

    commit

---
+++ my-page/src/components/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <div className="headbar">
+      <ol className="aaaaaa">
+        <h1>Diary</h1>
+        <li>
+          <a href="/menu123">하루</a>
+        </li>
+        <br />
+        <li>
+          <a href="/menu234">감사</a>
+        </li>
+        <br />
+        <li>
+          <a href="/menu3454">월간</a>
+        </li>
+      </ol>
+    </div>
+  );
+}
+
+export default Header;
