32a051f149913efa2617e01debc42ac7bac2c3ff
jjong <jjongseo6287@gmail.com>
2022-11-30 11:16:34 +0900

    잘못된 업로드 수정
    11/30

--- Front/src/components/Header.js
+++ Front/src/components/Header.js
@@ -1,20 +1,49 @@
-import React from "react";
-// import styled from "styled-components";
-// import { Routes, Route } from "react-router-dom";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+// import MenuIcon from '@mui/icons-material/Menu';
+import { createTheme } from '@material-ui/core/styles';
+// import { ThemeProvider } from '@material-ui/styles';
 
-function Header() {
+export default function Header() {
+  
+  
+  const theme = createTheme({
+    palette: {
+        primary: {
+          main: "#00ff0000",
+        },
+      }
+  });
+  
+
   return (
-    <div className="header">
-      <button>
-        <a href="/">C-Zero</a>
-      </button>
-      <a href="/feed">실천내용</a>
+    
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" theme={theme} >
+        <Toolbar >
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            {/* 로고 */}
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            실천내용
+          </Typography>
+          <Button color="inherit">로그인</Button>
+          <Button color="inherit" >회원가입</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
 
-          <a href="/login">로그인</a>
-
-      <a href="/join">회원가입</a>
-    </div>
   );
-}
 
-export default Header;
+}
