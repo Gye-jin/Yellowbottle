fb5b4913a7c1324ec4846baea154fe8287e8d725
Hyunio <hjizz1686@gmail.com>
2022-12-16 10:37:05 +0900

    댓글 업로드, 출력

--- Front/src/pages/board/DetailBoard.js
+++ Front/src/pages/board/DetailBoard.js
@@ -1,18 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { DetailBoardFetchData } from "../../Api/BoardData";
+import { DetailBoardFetchData, postComment } from "../../Api/BoardData";
 import Header from "../../components/header/Header";
 
+//게시글 상세보기
 const DetailBoard = () => {
+  //게시글
   const [board, setBoard] = useState([]);
+  //게시글 번호 가져오기
   const boardNo = useParams().boardNo;
+  //댓글 내용
+  const [commentContent, setCommentContent] = useState("");
+  //이동함수(추천게시물 이동에 사용)
   const navigate = useNavigate();
+
+  // 댓글내용 입력시 이벤트발생 ----수정고려중
+  const changeComment = (e) => {
+    setCommentContent(e.target.value);
+  };
+
+  // 댓글입력버튼 클릭 시 - 댓글내용폼데이터 형태로 백에 보냄
+  const createCommentData = () => {
+    let commentWriteData = new FormData();
+    const sessionId = sessionStorage.getItem("sessionId");
+    // console.log(sessionId);
+    commentWriteData.append("sessionId", sessionId);
+    commentWriteData.append("boardNo", boardNo);
+    commentWriteData.append("commentContent", commentContent);
+    postComment(commentWriteData, boardNo);
+  };
 
   useEffect(() => {
     const response = DetailBoardFetchData(boardNo);
     response.then((data) => setBoard(data));
   }, []);
   console.log(board);
+
   return (
     <>
       <Header />
@@ -32,6 +55,7 @@ const DetailBoard = () => {
                     alt="boardimage"
                   />
                 ))}
+
               <div>
                 <h3>
                   <span>조회수 : {board.viewCount}</span>
@@ -39,7 +63,7 @@ const DetailBoard = () => {
                 <div>{board.boardContent}</div>
               </div>
             </div>
-            {/* {console.log(board.comments)} */}
+            {/* 댓글 불러오기 */}
             {board.comments &&
               board.comments.map((comment) => (
                 <div>
@@ -47,6 +71,14 @@ const DetailBoard = () => {
                   <span>{comment.commentContent}</span>
                 </div>
               ))}
+            {/* 댓글 입력창 */}
+            <input
+              onChange={changeComment}
+              className="Comment-write"
+              placeholder="댓글을 입력해주세요!"
+              id="commentinput"
+            />
+            <button onClick={createCommentData}>댓글작성</button>
             <button onClick={() => navigate(`/recommendBoard/${boardNo}`)}>
               →
             </button>
@@ -58,4 +90,5 @@ const DetailBoard = () => {
     </>
   );
 };
+
 export default DetailBoard;
