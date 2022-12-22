fab8953e2d0fe419a11a9c9f77901f27c7ce4489
jjong <jjongseo6287@gmail.com>
2022-12-22 16:49:25 +0900

    게시물 작성 모달 수정

--- Front/src/components/comment/ModalForComment.js
+++ Front/src/components/comment/ModalForComment.js
@@ -29,9 +29,9 @@ const ModalForComment = ({ comment }) => {
           content: {
             position: "absolute",
             top: "30%",
-            left: "40%",
+            left: "44%",
             right: "40%",
-            bottom: "45%",
+            bottom: "48%",
             border: "1px solid lightgray",
             background: "#fff",
             overflow: "auto",
